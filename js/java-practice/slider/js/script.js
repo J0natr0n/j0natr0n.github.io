34890e4652c16bd97cbca5ad093b863ce81e7c6c
J0natr0n <oritz6499@gmail.com>
2015-11-02 19:35:41 -0800

    asdf

---
+++ js/java-practice/slider/js/script.js
@@ -0,0 +1,56 @@
+$(document).ready(function (){
+    //set options
+    var speed = 500;            //fade speed
+    var autoswitch = true;      //auto slider option
+    var autoswitch_speed = 4000 //auto slider speed
+    
+    //add inital active class
+    $('.slide').first().addClass('active');
+    
+    //hide all slides
+    $('.slide').hide();
+    
+    //show first slide
+    $('.active').show();
+    
+    //next handler
+    $('#next').on('click', nextSlide);
+    
+    //prev handler
+    $('#prev').on('click', prevSlide);
+    
+    //auto slide handler
+    if(autoswitch == true) {
+        setInterval(nextSlide,autoswitch_speed);
+    }
+    
+    //switch to next slide
+    function nextSlide() {
+        $('.active').removeClass('active').addClass('oldActive');
+        if($('.oldActive').is(':last-child')){
+            $('.slide').first().addClass('active');
+            
+        } else {
+            $('.oldActive').next().addClass('active');
+        }
+        
+        $('.oldActive').removeClass('oldActive');
+        $('.slide').fadeOut(speed);
+        $('.active').fadeIn(speed);
+    }
+    
+    //switch to prev slide
+    function prevSlide() {
+        $('.active').removeClass('active').addClass('oldActive');
+        if($('.oldActive').is(':first-child')){
+            $('.slide').last().addClass('active');
+            
+        } else {
+            $('.oldActive').prev().addClass('active');
+        }
+        
+        $('.oldActive').removeClass('oldActive');
+        $('.slide').fadeOut(speed);
+        $('.active').fadeIn(speed);
+    }
+});
