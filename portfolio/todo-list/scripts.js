c8374baafe6ebec2684c35f296eba64c12e9b6f7
J0natr0n <oritz6499@gmail.com>
2015-11-16 14:12:32 -0800

    todo success box

--- portfolio/todo-list/scripts.js
+++ portfolio/todo-list/scripts.js
@@ -33,7 +33,7 @@ function addComment(e){
     
     
     //step two; decorate the element
-    newItem.innerHTML = '<div>' + '<input type=\"checkbox\" id=\"check1\" />' + '<label for=\"check1\">'+ userNameValue + '</label>' + ' ' + commentValue +  '</div>';
+    newItem.innerHTML = '<div class=\"alert alert-success\">' + '<input type=\"checkbox\" id=\"check1\" />' + '<label for=\"check1\">'+ userNameValue + '</label>' + ' ' + commentValue +  '</div>';
     
     
     //step three; insert the element
